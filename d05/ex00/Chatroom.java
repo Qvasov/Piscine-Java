7062e83be4e2810ac9ca279e3fdaf7359b4e5a48
Qvasov <Qvasov@gmail.com>
2021-08-30 21:06:52 +0300

    d05 start

---
+++ d05/ex00/Chatroom.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class Chatroom {
+	private Long id;
+	private String name;
+	private User owner;
+	private List<Message> messages;
+}
---
+++ d05/ex00/Message.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Message {
+	private Long id;
+	private User author;
+	private Chatroom room;
+	private String text;
+	private Date date;
+}
