3fb32a92db4eda4db256ea23cb23700c7e604cc3
dbennie <dbennie@student.21-school.ru>
2021-09-03 20:26:32 +0300

    d08 ex01 findAll

--- d08/ex01/Service/src/main/java/school21/spring/service/repositories/UsersRepositoryJdbcTemplateImpl.java
+++ d08/ex01/Service/src/main/java/school21/spring/service/repositories/UsersRepositoryJdbcTemplateImpl.java
@@ -1,16 +1,22 @@
 package school21.spring.service.repositories;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import school21.spring.service.model.User;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public class UsersRepositoryJdbcTemplateImpl implements UsersRepository {
 
 	private NamedParameterJdbcTemplate jdbcTemplate;
+
+	private RowMapper<User> USER_ROW_MAPPER = (resultSet, i) ->
+			new User(resultSet.getLong("id"), resultSet.getString("email"));
 
 	public UsersRepositoryJdbcTemplateImpl(DataSource dataSource) {
 		this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
@@ -23,8 +29,7 @@ public class UsersRepositoryJdbcTemplateImpl implements UsersRepository {
 
 	@Override
 	public List<User> findAll() {
-		jdbcTemplate.query("SELECT * FROM users;", new BeanPropertyRowMapper<>(User.class));
-		return null;
+		return jdbcTemplate.query("SELECT * FROM users;", new BeanPropertyRowMapper<>(User.class));
 	}
 
 	@Override
