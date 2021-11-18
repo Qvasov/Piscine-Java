ed2398e412aab68c911c2fe7dc713a36ceb6ed65
Qvasov <Qvasov@gmail.com>
2021-11-18 23:10:25 +0300

    d07

--- d07/ex00/Reflection/src/main/java/edu/school21/forms/Program.java
+++ d07/ex00/Reflection/src/main/java/edu/school21/forms/Program.java
@@ -14,7 +14,6 @@ import java.util.stream.Collectors;
 public class Program {
 	private static final String PACKAGE_NAME = "edu.school21.classes";
 
-
 	public static void main(String[] args) throws Throwable {
 		Scanner scanner = new Scanner(System.in);
 		Map<String, Class> allClasses = findClasses();
@@ -36,92 +35,6 @@ public class Program {
 		instance = createObject(scanner, choosedClass);
 		updateObject(scanner, choosedClass, instance);
 		methodCall(scanner, choosedClass, instance);
-	}
-
-	private static void methodCall(Scanner scanner, Class choosedClass, Object instance) throws InvocationTargetException, IllegalAccessException {
-		System.out.println("---------------------");
-		System.out.println("Enter name of the method for call:");
-		String methodName = scanner.nextLine().trim();
-		for (Method method : choosedClass.getDeclaredMethods()) {
-			StringBuilder declaredMethod = new StringBuilder();
-			declaredMethod.append(method.getName())
-					.append("(")
-					.append(Arrays.stream(method.getParameterTypes())
-							.map(Class::getSimpleName)
-							.collect(Collectors.joining(", ")))
-					.append(")");
-
-			if (declaredMethod.toString().equals(methodName)) {
-				method.setAccessible(true);
-				Object result = method.invoke(instance, Arrays.stream(method.getParameterTypes())
-						.map(type -> {
-							System.out.println("Enter " + type.getSimpleName() + " value:");
-							return getValue(scanner.nextLine().trim(), type);
-						})
-						.toArray());
-
-				if (result != null) {
-					System.out.println("Method returned:");
-					System.out.println(result);
-				}
-
-				return;
-			}
-		}
-		throw new IllegalAccessException("Method " + methodName + " not found");
-	}
-
-	private static void updateObject(Scanner scanner, Class choosedClass, Object instance) throws IllegalAccessException {
-		System.out.println("---------------------");
-		System.out.println("Enter name of the field for changing:");
-		String fieldName = scanner.nextLine().trim();
-		for (Field field : choosedClass.getDeclaredFields()) {
-			if (field.getName().equals(fieldName)) {
-				System.out.println("Enter " + field.getType().getSimpleName() + " value:");
-				fieldName = scanner.nextLine().trim();
-				field.setAccessible(true);
-				field.set(instance, getValue(fieldName, field.getType()));
-				System.out.println("Object updated: " + instance.toString());
-				return;
-			}
-		}
-		throw new IllegalAccessException("Field " + fieldName + " not found");
-	}
-
-	private static Object createObject(Scanner scanner, Class choosedClass)
-			throws InvocationTargetException, InstantiationException, IllegalAccessException {
-		Object instance = null;
-		for (Constructor c : choosedClass.getConstructors()) {
-			if (c.getParameterTypes().length == 0) {
-				instance = c.newInstance();
-				break;
-			}
-		}
-		System.out.println("---------------------");
-		System.out.println("Let's create an object.");
-		for (Field field : choosedClass.getDeclaredFields()) {
-			System.out.println(field.getName() + ":");
-			String input = scanner.nextLine().trim();
-			field.setAccessible(true);
-			field.set(instance, getValue(input, field.getType()));
-		}
-		System.out.println("Object created: " + instance.toString());
-		return instance;
-	}
-
-	private static Object getValue(String input, Class type) {
-		if (type.equals(String.class)) {
-			return input;
-		} else if (type.equals(int.class) || type.equals(Integer.class)) {
-			return Integer.parseInt(input);
-		} else if (type.equals(double.class) || type.equals(Double.class)) {
-			return Double.parseDouble(input);
-		} else if (type.equals(boolean.class) || type.equals(Boolean.class)) {
-			return Boolean.parseBoolean(input);
-		} else if (type.equals(long.class) || type.equals(Long.class)) {
-			return Long.parseLong(input);
-		}
-		return Object.class;
 	}
 
 	private static Map<String, Class> findClasses() {
@@ -176,4 +89,89 @@ public class Program {
 		System.out.println(fieldsAndMethods);
 	}
 
+	private static Object createObject(Scanner scanner, Class choosedClass)
+			throws InvocationTargetException, InstantiationException, IllegalAccessException {
+		Object instance = null;
+		for (Constructor c : choosedClass.getConstructors()) {
+			if (c.getParameterTypes().length == 0) {
+				instance = c.newInstance();
+				break;
+			}
+		}
+		System.out.println("---------------------");
+		System.out.println("Let's create an object.");
+		for (Field field : choosedClass.getDeclaredFields()) {
+			System.out.println(field.getName() + ":");
+			String input = scanner.nextLine().trim();
+			field.setAccessible(true);
+			field.set(instance, getValue(input, field.getType()));
+		}
+		System.out.println("Object created: " + instance.toString());
+		return instance;
+	}
+
+	private static void updateObject(Scanner scanner, Class choosedClass, Object instance) throws IllegalAccessException {
+		System.out.println("---------------------");
+		System.out.println("Enter name of the field for changing:");
+		String fieldName = scanner.nextLine().trim();
+		for (Field field : choosedClass.getDeclaredFields()) {
+			if (field.getName().equals(fieldName)) {
+				System.out.println("Enter " + field.getType().getSimpleName() + " value:");
+				fieldName = scanner.nextLine().trim();
+				field.setAccessible(true);
+				field.set(instance, getValue(fieldName, field.getType()));
+				System.out.println("Object updated: " + instance.toString());
+				return;
+			}
+		}
+		throw new IllegalAccessException("Field " + fieldName + " not found");
+	}
+
+	private static void methodCall(Scanner scanner, Class choosedClass, Object instance) throws InvocationTargetException, IllegalAccessException {
+		System.out.println("---------------------");
+		System.out.println("Enter name of the method for call:");
+		String methodName = scanner.nextLine().trim();
+		for (Method method : choosedClass.getDeclaredMethods()) {
+			StringBuilder declaredMethod = new StringBuilder();
+			declaredMethod.append(method.getName())
+					.append("(")
+					.append(Arrays.stream(method.getParameterTypes())
+							.map(Class::getSimpleName)
+							.collect(Collectors.joining(", ")))
+					.append(")");
+
+			if (declaredMethod.toString().equals(methodName)) {
+				method.setAccessible(true);
+				Object result = method.invoke(instance, Arrays.stream(method.getParameterTypes())
+						.map(type -> {
+							System.out.println("Enter " + type.getSimpleName() + " value:");
+							return getValue(scanner.nextLine().trim(), type);
+						})
+						.toArray());
+
+				if (result != null) {
+					System.out.println("Method returned:");
+					System.out.println(result);
+				}
+
+				return;
+			}
+		}
+		throw new IllegalAccessException("Method " + methodName + " not found");
+	}
+
+	private static Object getValue(String input, Class type) {
+		if (type.equals(String.class)) {
+			return input;
+		} else if (type.equals(int.class) || type.equals(Integer.class)) {
+			return Integer.parseInt(input);
+		} else if (type.equals(double.class) || type.equals(Double.class)) {
+			return Double.parseDouble(input);
+		} else if (type.equals(boolean.class) || type.equals(Boolean.class)) {
+			return Boolean.parseBoolean(input);
+		} else if (type.equals(long.class) || type.equals(Long.class)) {
+			return Long.parseLong(input);
+		}
+		return Object.class;
+	}
 }
